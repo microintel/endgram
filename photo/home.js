0b7c201ee9a23660bdfe06b1a74dec28a8668c6e
Microintel <135629446+microintel@users.noreply.github.com>
2024-08-11 17:14:32 +0530

    Update home.js

--- photo/home.js
+++ photo/home.js
@@ -36,7 +36,7 @@ document.getElementById('cmts').src="https://img.icons8.com/?size=100&id=2837&fo
 
 let infi=100;
 async function loadimg(){
-let fetd= await fetch("https://api.github.com/repos/microintel/Resources/contents/photo");
+let fetd= await fetch("https://api.github.com/repos/microintel/endgram/contents/photo");
 let con=await fetd.json();
 document.getElementById('load').style.display='none';
 
